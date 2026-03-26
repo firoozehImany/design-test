7c6c3422aa8c1d6b35339b82080282e445860d61
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2026-03-26 16:52:43 +0100

    002

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import CountdownContainer from "./CountdownContainer";
 
 export default function Hero() {
   return (
-    <div className="flex-1 flex flex-col items-center text-center min-h-[480px]">
-      <h1 className="text-center mt-[138px] mb-[52px] md:mt-[135px] md:mb-[104px] px-4 font-light uppercase text-typo tracking-[0.34em] md:tracking-[0.39em] text-[18px] md:text-[21px]">
+    <div className="flex-1 flex flex-col items-center text-center">
+      <h1 className="text-center mt-[138px] mb-[52px] md:mt-[130px] md:mb-[104px] px-4 font-light uppercase text-typo tracking-[0.34em] md:tracking-[0.39em] text-[18px] md:text-[21px]">
         We're launching soon
       </h1>
       <CountdownContainer />
